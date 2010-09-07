9ad962e1656d75039d4cc204156374c3befcbaeb
Eric Draut <edraut@gmail.com>
2010-09-07 12:58:53 -0600

    added missing rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyesWin::Application.config.secret_token = '7e2f54802166d9c4052cd145bcf79519bcb9c97356c1071cf002eeb903d7ff8f8936f87d8ca4a9168087dd92023a10972ad34d9b54b50761ab4ccf2d5bb63c8c'
